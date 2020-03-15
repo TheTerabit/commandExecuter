e5d233bff765a52af6cf9b35ef8bd28d540a9e4e
TheTerabit <terrabitroman@gmail.com>
2020-03-15 20:07:49 +0100

    github calendar text display

--- src/App.scala
+++ src/App.scala
@@ -1,3 +1,5 @@
+import java.time.{LocalDate, LocalDateTime}
+
 import scala.language.postfixOps
 object App {
 
@@ -7,31 +9,71 @@ object App {
 
 
   def main(args: Array[String]): Unit ={
+    print("Enter your caption: ")
     val caption = scala.io.StdIn.readLine().toUpperCase();//input
-    fillGithubHistory(letterParser.parseString(caption));//output
+    print("Root password: ")
+    val password = scala.io.StdIn.readLine();//input
+    fillGithubHistory(letterParser.parseString(caption), password);//output
   }
 
-  private def fillGithubHistory(githubSquares: List[Int]): Unit ={
-      /*//print
-      var counter=0
-      for(i<-githubSquares){
-        if(i==1)
-          print(219.toChar)
-        else
-          print(' ')
-        counter=(counter+1)%7
-        if(counter==0)
-          println
-      }
-       */
-
-    cmdLine.configureGithub();
-    while(!timeChecker.sunday()){Thread.sleep(60000)}//wait until first Sunday
+  private def fillGithubHistory(githubSquares: List[Int], password: String): Unit ={
+    /*//print
+    var counter=0
     for(i<-githubSquares){
       if(i==1)
-        cmdLine.colorSquare()
-        do {Thread.sleep(60000)} while(!timeChecker.newDay())
+        print(219.toChar)
+      else
+        print(' ')
+      counter=(counter+1)%7
+      if(counter==0)
+        println
     }
+     */
+    val now = LocalDate.now()
+    cmdLine.configureGithub();
+    var currentDate = cmdLine.prepareDate(password)
+    for (i <- githubSquares){
+      print(i)
+      if(i==1) {
+        var numOfCommits = getNumberOfCommits(currentDate)
+        println(numOfCommits)
+        println("-------")
+        for (j <- 0 until numOfCommits) {
+          cmdLine.edit(j, cmdLine.createDateCode(currentDate))
+          //updateFile()
+          cmdLine.commit()
+        }
+
+      }
+      currentDate = currentDate.plusDays(1)
+      cmdLine.nextDate(password, currentDate)
+    }
+
+    cmdLine.nextDate(password, now)
+    //cmdLine.configureGithub();
+    //while(!timeChecker.sunday()){Thread.sleep(60000)}//wait until first Sunday//go to begining
+    //goToFirstSquare()
+    /*for(i<-githubSquares){
+      //if(i==1)
+        cmdLine.colorSquare()
+      //moveDate()
+      return;
+    }
+
+     */
   }
 
+  private def getNumberOfCommits(date: LocalDate): Int ={
+    println(date.getDayOfWeek.toString)
+    val currentDay = date.getDayOfWeek.getValue
+    if(currentDay==7 || currentDay==1)
+      return 8
+    else if(currentDay==2 || currentDay==3)
+      return 6
+    else if(currentDay==4 || currentDay==5)
+      return 4
+    else
+      return 2
+
+  }
 }
--- src/CmdLine.scala
+++ src/CmdLine.scala
@@ -1,5 +1,7 @@
 
 import java.io.{File, PrintWriter}
+import java.time.{Clock, Instant, LocalDate, LocalDateTime, ZoneOffset, ZonedDateTime}
+import java.util.{Calendar, Date, Objects}
 
 import sys.process._
 import scala.language.postfixOps
@@ -7,37 +9,82 @@ import scala.language.postfixOps
 
 class CmdLine(){
 
-  val path = new File(".").getAbsolutePath() + "\\Repository";
+  //val path = new File(".").getAbsolutePath() + "//Repository";
+  val path = "/home/bartek/Desktop/commandExecuter/Repository";
   val timeChecker = new TimeChecker
 
 
   def configureGithub(): Unit ={
-    val cmd1="git -C " + path + " config user.email theterabit@gmail.com"
-    val cmd2="git -C " + path + " config user.email"
-    val cmd3="git -C " + path + " remote add origin https://github.com/CommandLineApp/Repository.git"
-    val result = cmd1 #| cmd2 #| cmd3 !
+    val cmd2="git -C "+path+" config user.email theterabit@gmail.com"
+    val cmd3="git -C "+path+" config user.email"
+    val cmd4="git -C "+path+" remote add origin https://github.com/CommandLineApp/Repository.git"
+    val cmd1="git -C "+path+" init"
+    val cmd5 = "git -C "+path+" add ."
+    val result = cmd1 #| cmd2 #| cmd3 #| cmd4 #| cmd5 !
   }
 
+  def prepareDate(password: String): LocalDate = {
+    val localDate = getDate()
+    val dateCode = createDateCode(localDate)
+    changeDate(password, dateCode)
+    return localDate
+  }
+  def changeDate(password: String, dateCode: String): Unit = {
+    val cmd1="echo "+ password
+    val cmd = "sudo -S date +%Y%m%d -s \""+dateCode+"\""
+    val cmd2 = "date"
+    val result = cmd1 #| cmd !
+  }
+  def getDate(): LocalDate =
+  {
+    var local: LocalDate = LocalDate.now()
+    local = local.minusYears(1)
+    //local = local.plusMonths(1)
+
+    while(!local.getDayOfWeek().toString.equals("SUNDAY")){
+      local = local.plusDays(1)
+    }
+    print(local.getDayOfWeek)
+    return local
+
+  }
+  def nextDate(password: String, currentDate: LocalDate): Unit = {
+    val dateCode = createDateCode(currentDate)
+    changeDate(password, dateCode)
+  }
+
+  def createDateCode(local: LocalDate): String = {
+    var monthCode = local.getMonthValue.toString
+    var dayCode = local.getDayOfMonth.toString
+    if(monthCode.length<2)
+      monthCode = "0" +monthCode
+    if(dayCode.length<2)
+      dayCode = "0" +dayCode
+    return local.getYear.toString + monthCode + dayCode
+  }
 
   def colorSquare(): Unit ={
+
     for(numberOfCommit <- 1 to timeChecker.getNumberOfCommits()){
       edit(numberOfCommit, timeChecker.getDate())
       commit()
     }
   }
 
-  private def edit(numberOfCommit: Int, date: String): Unit ={
-    val writer = new PrintWriter(new File(path + "\\file.txt"))
+  def edit(numberOfCommit: Int, date: String): Unit ={
+    val writer = new PrintWriter(new File(path + "/file.txt"))
     writer.write(numberOfCommit+"_"+date)
     writer.close()
   }
 
-  private def commit(): Unit ={
-    println("commited")
+  def commit(): Unit ={
+    val path=new File(".").getAbsolutePath()+"/Repository ";
+    val cmd="git -C "+path+"commit -m \"nazwaTestowa\" -a"
+    val exitCode = cmd.!
   }
 
 
-/*
+
   def executeCommand(): Unit ={
     val path=new File(".").getAbsolutePath()+"\\Repository ";
     val cmd="git -C "+path+"status"
@@ -51,6 +98,6 @@ class CmdLine(){
     val result = cmd2 #| cmd3 #| cmd6 #| cmd4 #| cmd5 #| cmd7!
   }
 
- */
+
 
 }
